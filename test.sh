8bbeab610985e0cbda80117874091281241dbf01
Max Starr <max@maxstarr.xyz>
2023-08-25 10:22:53 -0500

    init

---
+++ test.sh
@@ -0,0 +1,8 @@
+npx web-push send-notification \
+  --endpoint=https://updates.push.services.mozilla.com/wpush/v2/gAAAAABkuXIbgQ7DHz2ciTZjveSE_783IcPvf6VaEyqtqwXuMk1iV-65PkBPFzj7NYyhLsUYmAQjW1CDmRmzO_c-96-B-QNpV08rXna4QIkucH2ta19roxHCXEVcEktK5jbohuGI2787TQ35W5hMWG9ufhqwDi4We0W0DY0oDEW-vloUDnAo0-c \
+  --key=BO3M_Binquj9J2B4Oj1ZvS4Y5F844nATW1Nj6wA79ZDPRLtednq3ccwf5C90aKpxU5dZotRhdWvWNYOlN82u5Bg \
+  --auth=TclbRGM-1uSz8qRxdpk_HQ \
+  --vapid-subject=mailto:example@qq.com \
+  --vapid-pubkey=BDciDDwLXqIrpxUXH-6dy75kLyEF2WFE6uVX-0PAYu7kssthiiG--lrO_mg5Q3cQzstASV2aqo-KbcB95HQ7MX8 \
+  --vapid-pvtkey=YLZwlErn-oi6kK3Xe9OeFN763clMD_KgOugy9hc3qTo \
+  --payload=Hello
