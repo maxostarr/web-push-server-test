afbdef6092de3b773fc0151d835a967e8815c171
Max Starr <max@maxstarr.xyz>
2023-08-25 15:31:54 -0500

    add icons and such

--- vite.config.ts
+++ vite.config.ts
@@ -25,9 +25,26 @@ export default defineConfig({
         description: 'A Vite PWA project',
         icons: [
           {
-            src: '/logo.png',
-            sizes: '262x227',
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
             type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       },
